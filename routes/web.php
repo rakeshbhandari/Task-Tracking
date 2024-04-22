547c3afb6fb3402c60a49c236dcefedc78787417
Rakesh <rakeshbhandari@gmail.com>
2024-04-22 15:13:07 +0545

    first

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Task;
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,17 +16,23 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     return view(
         'index',
-        [
-            'tasks' => Task::latest()->get()
-        ]
+        ['tasks' => Task::latest()->get()]
     );
 })->name('tasks.index');
 
+
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
+    
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => Task::findorFail($id)]);
 })->name('tasks.show');
 
 
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 
 
