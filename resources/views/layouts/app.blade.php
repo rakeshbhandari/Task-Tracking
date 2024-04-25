f29da6d4ae8a11fd65209ae7e9e34be0f472a5f2
Rakesh <rakeshbhandari@gmail.com>
2024-04-25 14:07:18 +0545

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <title>Daily Task Planner</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     @yield('styles')
 
@@ -13,22 +14,26 @@
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg ">
 
-    <h1 class="text-5xl mt-4 mb-4">
-        @yield('title')
-    </h1>
+    <h1 class="text-5xl mt-4 mb-4"> @yield('title') </h1>
+    <div x-data="{ flash : true }">
 
 
-    <div>
+        @if (session()->has('success'))
 
-        <!-- @if (session()->has('success')) -->
-        <!-- <div>{{ session('success') }}</div> -->
-        <!-- @endif -->
-
-        <div class="mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700">
+        <div x-show="flash"
+            class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700">
             <strong class="font-bold">Success!</strong>
-            <div>This is a flash message.</div>
+            <div>{{ session('success') }}</div>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
         </div>
+        @endif
         @yield('content')
+
     </div>
 </body>
 
